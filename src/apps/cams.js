9d94ef89c6025cdcb6c77f2e2828d437e101b354
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-07-12 20:10:30 -0400

    ok

--- src/apps/cams.js
+++ src/apps/cams.js
@@ -1,18 +1,16 @@
-//Camera Authentication
-var ip_address = '190.54.179.123';
-//camera username and password
-var username = 'admin';
-var password = 'NutrAdm_20.20';
-// rtsp://admin:NutrAdm_20.20@190.54.179.123:8082/cam/realmonitor?channel=1>&subtype=1
 //A channel of camera stream
 const Stream = require('node-rtsp-stream');
 
 const cams = [
 	{
-		streamUrl:
-			'rtsp://admin:NutrAdm_20.20@190.54.179.123:8082/cam/realmonitor?channel=1>&subtype=1',
+		streamUrl: `rtsp://admin:NutrAdm_20.20@190.54.179.123:8082/cam/realmonitor?channel=1>&subtype=1`,
 		wsPort: 9999,
+		nombre: 'camara',
 	},
 ];
 
-const stream = cams.map((cam) => new Stream(cam));
+const streams = cams.map((cam) => new Stream(cam));
+
+// streams[0].stop();
+
+// streams.map((stream) => stream.stop());
